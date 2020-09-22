23432273ebde00e7667fd9abc11803df55905f89
pi <pi@raspberry>
2020-09-22 23:00:56 +0200

    add load and save configuration

--- app/camera.py
+++ app/camera.py
@@ -1,7 +1,29 @@
 import picamera
+import config
 
 
 RESOLUTION = (1280, 720)
 FRAME_RATE = 30
 
-camera = picamera.PiCamera(resolution=RESOLUTION, framerate=FRAME_RATE)
+camera = None
+
+
+def load_config():
+    global RESOLUTION, FRAME_RATE, camera
+    configuration = config.load()
+    configuration["resolution"] = "x".join([str(x) for x in RESOLUTION])
+    RESOLUTION = tuple([int(i) for i in configuration["resolution"].split("x")])
+    FRAME_RATE = configuration["framerate"]
+    camera = picamera.PiCamera(resolution=RESOLUTION, framerate=FRAME_RATE)
+    camera.rotation = configuration.get("rotation") or 0
+    camera.annotate_background = picamera.Color('grey')
+
+def save_config():
+    configuration = dict()
+    configuration["resolution"] = "x".join([str(x) for x in RESOLUTION])
+    configuration["framerate"] = FRAME_RATE
+    configuration["rotation"] = camera.rotation
+    config.save(configuration)
+
+
+load_config()
