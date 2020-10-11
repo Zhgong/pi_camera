96908c0016a31c80332e86b24cbc3973ab8b808d
pi <pi@raspberry>
2020-10-11 21:05:07 +0200

    use base64 for image

--- app/app_dash.py
+++ app/app_dash.py
@@ -7,6 +7,7 @@ import camera
 import io
 from flask import Flask, Response
 from time import sleep
+import base64
 
 server = Flask(__name__)
 
@@ -23,24 +24,44 @@ def video_feed():
 
 
 def gen():
-    yield from stream(camera.camera)
+    return stream(camera.camera)
 
 
 def stream(camera):
-    while True:
-        stream = io.BytesIO()
-        camera.capture(stream, format='jpeg', use_video_port=True)
-        stream.seek(0)
-        frame = stream.read()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        sleep(1/25)
+    stream = io.BytesIO()
+    camera.capture(stream, format='jpeg', use_video_port=True)
+    stream.seek(0)
+    frame = stream.read()
+    return (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def image(camera):
+    stream = io.BytesIO()
+    camera.capture(stream, format='jpeg', use_video_port=True)
+    stream.seek(0)
+    frame = stream.read()
+    return frame
 
 
 app.layout = html.Div([
     html.H1('Dash Camera'),
-    html.Img(src='/video_feed')
+    html.Button('Send',id='btn-send', n_clicks=0),
+    html.Img(id='img', src='')
 ])
+
+
+@app.callback(
+    Output('img', 'src'),
+    Input('btn-send','n_clicks')
+)
+def send(n_clicks):
+    print(n_clicks)
+    img = image(camera.camera)
+    # print(img)
+    encode = base64.b64encode(img).decode('utf-8')
+    # print(encode)
+    return 'data:image/jpeg;base64,{}'.format(encode)
+
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=False)
