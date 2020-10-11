9080735347eb119ee9dbd53fd928e419226e69a6
pi <pi@raspberry>
2020-10-11 23:46:13 +0200

    refactor code

---
+++ app/api.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, Response
+import io
+from time import sleep
+import logging
+
+from flask.json import jsonify
+import camera
+import continues
+from threading import Thread
+
+
+from picamera import exc
+logger = logging.getLogger()
+
+
+app = Flask(__name__)
+
+
+def gen():
+    yield from stream(camera.camera)
+
+
+def stream(camera):
+    while True:
+        stream = io.BytesIO()
+        camera.capture(stream, format='jpeg', use_video_port=True)
+        stream.seek(0)
+        frame = stream.read()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        sleep(1/25)
+
+
+@app.route("/camera/rotate")
+def camera_rotate():
+    previous_rotation = camera.camera.rotation
+    camera.camera.rotation += 90
+    return jsonify({"status": "success", "previous_rotation": previous_rotation, "current_rotation": camera.camera.rotation})
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/config')
+def get_config():
+    return jsonify(camera.configuration)
--- app/streaming.py
+++ app/streaming.py
@@ -7,54 +7,17 @@ from flask.json import jsonify
 import camera
 import continues
 from threading import Thread
+from api import app
 
 
 from picamera import exc
 logger = logging.getLogger()
-
-RESOLUTION = (1280, 720)
-FIRST_RUN = True
-_EXIT = False
-BUFFER_SIZE = 65  # seconds
-FRAME_RATE = 30
-
-app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-def gen():
-    yield from stream(camera.camera)
-
-
-def stream(camera):
-    while True:
-        stream = io.BytesIO()
-        camera.capture(stream, format='jpeg', use_video_port=True)
-        stream.seek(0)
-        frame = stream.read()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        sleep(1/25)
-
-@app.route("/camera/rotate")
-def camera_rotate():
-    previous_rotation = camera.camera.rotation 
-    camera.camera.rotation += 90
-    return jsonify({"status":"success", "previous_rotation":previous_rotation, "current_rotation": camera.camera.rotation})
-    
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/config')
-def get_config():
-    return jsonify(camera.configuration)
 
 def run():
     t_continues = Thread(target=continues.main, args=())
