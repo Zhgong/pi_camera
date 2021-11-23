fe7d247c7d50f22ea39a1835e8ed0affe14a80f8
zhou <zhou@zhou-3000053>
2021-11-23 23:03:13 +0100

    添加实时显示摄像头图像

--- app/static/js/app.js
+++
@@ -1,8 +0,0 @@
-console.log("Start of script");
-// import { MyController } from "./control";
-
-const btnStartRecording = document.querySelector("#startRecording");
-const doms = {btnStartRecording};
-const camera = new Camera()
-// const controller = new MyController(doms, camera);
-const controller = new MyController(doms);
